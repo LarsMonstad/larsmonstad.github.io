8a2b4ab68e1acd49294629ff25d2ef42e67db58a
Lars Løberg Monstad <lars.monstad@gmail.com>
2024-01-14 02:20:34 +0100

    Create script.js

---
+++ deploytest/script.js
@@ -0,0 +1,71 @@
+const CANVAS_SIZE = 280;
+const CANVAS_SCALE = 0.5;
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const clearButton = document.getElementById("clear-button");
+const predictionResult = document.getElementById("predictionResult");
+
+let isMouseDown = false;
+let lastX = 0;
+let lastY = 0;
+
+ctx.lineWidth = 28;
+ctx.lineJoin = "round";
+ctx.strokeStyle = "#212121";
+ctx.font = "28px sans-serif";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+ctx.fillStyle = "#212121";
+ctx.fillText("Draw here!", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    predictionResult.innerText = "Prediction will appear here";
+}
+
+function drawLine(fromX, fromY, toX, toY) {
+    ctx.beginPath();
+    ctx.moveTo(fromX, fromY);
+    ctx.lineTo(toX, toY);
+    ctx.closePath();
+    ctx.stroke();
+    // Update predictions here if needed
+    // updatePredictions();
+}
+
+function canvasMouseDown(event) {
+    isMouseDown = true;
+    const x = event.offsetX / CANVAS_SCALE;
+    const y = event.offsetY / CANVAS_SCALE;
+    lastX = x + 0.001;
+    lastY = y + 0.001;
+    canvasMouseMove(event);
+}
+
+function canvasMouseMove(event) {
+    const x = event.offsetX / CANVAS_SCALE;
+    const y = event.offsetY / CANVAS_SCALE;
+    if (isMouseDown) {
+        drawLine(lastX, lastY, x, y);
+    }
+    lastX = x;
+    lastY = y;
+}
+
+function bodyMouseUp() {
+    isMouseDown = false;
+    // Send image data for prediction here if real-time update is needed
+}
+
+function bodyMouseOut(event) {
+    if (!event.relatedTarget || event.relatedTarget.nodeName === "HTML") {
+        isMouseDown = false;
+    }
+}
+
+canvas.addEventListener("mousedown", canvasMouseDown);
+canvas.addEventListener("mousemove", canvasMouseMove);
+document.body.addEventListener("mouseup", bodyMouseUp);
+document.body.addEventListener("mouseout", bodyMouseOut);
+clearButton.addEventListener("mousedown", clearCanvas);
